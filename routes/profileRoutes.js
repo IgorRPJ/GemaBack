22ef4308917cbe75db83c14a912cc4f405b202a3
IgorRPJ <133509011+IgorRPJ@users.noreply.github.com>
2025-07-23 03:08:31 -0300

    Update profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    if (req.isAuthenticated()) {
-        res.json({ user: req.user })
-    } else {
-        res.status(401).json({ error: 'Não autorizado' })
-    }
+  console.log('isAuthenticated?', req.isAuthenticated());
+  console.log('req.user:', req.user);
+
+  if (req.isAuthenticated()) {
+    res.json({ user: req.user })
+  } else {
+    res.status(401).json({ error: 'Não autorizado' })
+  }
 })
+
 
 module.exports = router
